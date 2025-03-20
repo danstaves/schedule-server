261b0cb906e9085cc2ac3372439e8d3847f8e6a5
Dan <danstaves@gmail.com>
2025-03-19 21:20:46 -0400

    initialize server

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+app.use(express.static(path.join(__dirname, '../ai-scheduler/build')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../ai-scheduler/build', 'index.html'));
+});
+
+app.post('/api/getSchedule', upload.single("file"), (req, res) => {
+    const file = req.file;
+
+    console.log(`Received file: ${file.originalname}`);
+
+    res.sendStatus(200);
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    console.log('Press Ctrl+C to quit.');
+});
